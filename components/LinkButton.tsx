18b57ed10987fd6a98f767ac136ca77ce78cd667
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2024-08-10 23:56:31 +0530

    Update the LinkButton image size 🔥

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -13,8 +13,8 @@ const LinkButton = (props: Props) => {
   const { href, imageUrl, imageAlt, text } = props
   return (
     <Link href={href} target="_blank" rel="noreferrer">
-      <span className="ext-[#f2617b] inline-flex gap-2 items-baseline rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100">
-        <Image src={imageUrl} width={14} height={14} alt={imageAlt} />
+      <span className="ext-[#f2617b] inline-flex gap-1 items-baseline rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100">
+        <Image src={imageUrl} width={20} height={10} alt={imageAlt} />
         {text}
       </span>
     </Link>
