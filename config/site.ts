17059051f043f88dc69e32a6f5dcddfa352017f2
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2024-08-10 23:50:01 +0530

    Add site configurations 🔥

---
+++ config/site.ts
@@ -0,0 +1,20 @@
+export const siteConfig = {
+  name: "Lokeswaran Aruljothy",
+  role: "Software Engineer",
+  description: "Lokeswaran Aruljothy portfolio website",
+  myImage: "/lokeswaran.jpeg",
+  author: "Lokeswaran Aruljothy",
+  currentCompany: "Thoughtworks",
+  currentCompanyImage: "/thoughtworks-logo.png",
+  currentCompanyUrl: "https://www.thoughtworks.com",
+  email: "lokeswaranaruljothy@gmail.com",
+  links: {
+    resume:
+      "https://drive.google.com/file/d/1a6sP_y6cJ4w3dE8x_3OLyVHSOp18EV7V/view",
+    github: "https://github.com/lokeswaran-aj",
+    linkedin: "https://www.linkedin.com/in/lokeswaran-aj/",
+    twitter: "https://twitter.com/lokeswaran_aj",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
