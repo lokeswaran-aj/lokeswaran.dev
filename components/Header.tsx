373c00ab2a9bc017d1a18aac2636b3af93ff651d
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2024-08-11 12:48:41 +0530

    Update the space between the header buttons 💫

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <div className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none">
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-      <span className="inline-flex gap-20 cursor-pointer items-center justify-center rounded-full bg-gray-950 px-8 py-1 text-sm font-medium text-gray-50 backdrop-blur-3xl">
+      <span className="inline-flex w-72 cursor-pointer items-center justify-between rounded-full bg-gray-950 px-8 py-1 text-sm font-medium text-gray-50 backdrop-blur-3xl">
         <Link
           href="/"
           className={cn(
