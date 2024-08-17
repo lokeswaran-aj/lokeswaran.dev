973a7fb4dd6893d0487992789e82e93994e24196
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2024-08-17 14:09:52 +0530

    Update robots and sitemap☄️

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots() {
     rules: [
       {
         userAgent: "*",
+        allow: ["/"],
       },
     ],
     sitemap: `${siteConfig.links.url}/sitemap.xml`,
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,20 @@
 import { siteConfig } from "@/config/site"
 import getBlogPosts from "@/lib/blog"
+import { MetadataRoute } from "next"
 
-export default async function sitemap() {
-  let blogs = getBlogPosts().map((blog) => ({
+export default function sitemap(): MetadataRoute.Sitemap {
+  let blogs: MetadataRoute.Sitemap = getBlogPosts().map((blog) => ({
     url: `${siteConfig.links.url}/blog/${blog.slug}`,
     lastModified: blog.metadata.publishedAt,
+    changeFrequency: "monthly",
+    priority: 0.9,
   }))
 
-  let routes = ["", "/blog", "/work"].map((route) => ({
+  let routes: MetadataRoute.Sitemap = ["", "/blog", "/work"].map((route) => ({
     url: `${siteConfig.links.url}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
+    changeFrequency: "monthly",
+    priority: 1,
   }))
 
   return [...routes, ...blogs]
