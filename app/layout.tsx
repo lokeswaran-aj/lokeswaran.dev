3f5a780309b4824592e337d3ffc3eff51af6ef30
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2024-09-08 20:21:23 +0530

    Add canonical link in the metadata 🚀

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata, Viewport } from "next"
-import { GeistSans } from "geist/font/sans"
-import { Fira_Code } from "next/font/google"
-import "./globals.css"
-import { cn } from "@/lib/utils"
-import { siteConfig } from "@/config/site"
-import Header from "@/components/Header"
-import { Analytics } from "@vercel/analytics/react"
-import Providers from "@/components/Providers"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import type { Metadata, Viewport } from 'next'
+import { GeistSans } from 'geist/font/sans'
+import { Fira_Code } from 'next/font/google'
+import './globals.css'
+import { cn } from '@/lib/utils'
+import { siteConfig } from '@/config/site'
+import Header from '@/components/Header'
+import { Analytics } from '@vercel/analytics/react'
+import Providers from '@/components/Providers'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
-const FiraCode = Fira_Code({ subsets: ["latin"], variable: "--fira-code" })
+const FiraCode = Fira_Code({ subsets: ['latin'], variable: '--fira-code' })
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -20,13 +20,16 @@ export const metadata: Metadata = {
     template: `%s | ${siteConfig.author}`,
   },
   description: siteConfig.description,
+  alternates: {
+    canonical: siteConfig.links.url,
+  },
   openGraph: {
     title: siteConfig.name,
     description: siteConfig.description,
     url: siteConfig.links.url,
     siteName: siteConfig.name,
-    locale: "en_US",
-    type: "website",
+    locale: 'en_US',
+    type: 'website',
   },
   robots: {
     index: true,
@@ -34,24 +37,24 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
     },
   },
   twitter: {
     title: siteConfig.name,
-    card: "summary_large_image",
+    card: 'summary_large_image',
   },
   verification: {
-    google: "p1iiyT6ruqWpFWpJlMQzvcSP75S-4eL8eyNZhOT82ww",
+    google: 'p1iiyT6ruqWpFWpJlMQzvcSP75S-4eL8eyNZhOT82ww',
   },
 }
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
   ],
 }
 
@@ -61,19 +64,19 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={cn(
-          "antialiased bg-background text-text max-w-2xl  mx-12 sm:mx-auto",
+          'antialiased bg-background text-text max-w-2xl  mx-12 sm:mx-auto',
           GeistSans.className,
           FiraCode.variable
         )}
       >
         <Providers>
-          <main className="flex flex-col py-20 min-h-[calc(100dvh-3rem)]">
+          <main className='flex flex-col py-20 min-h-[calc(100dvh-3rem)]'>
             {children}
           </main>
-          <div className="fixed inset-x-0 bottom-0 z-20 mx-auto mb-4 w-screen flex justify-center h-12 px-6">
+          <div className='fixed inset-x-0 bottom-0 z-20 mx-auto mb-4 w-screen flex justify-center h-12 px-6'>
             <Header />
           </div>
           <Analytics />
